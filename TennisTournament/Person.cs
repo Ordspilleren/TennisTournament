6d1e50af00f8952030228853cd51c48fd502098f
Lasse Bjerre <lasse@lgb.dk>
2016-02-11 12:03:08 +0100

    Added basic classes and minor functionality

---
+++ TennisTournament/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisTournament
+{
+    public enum Gender { Male, Female }
+
+    public interface IPerson
+    {
+        string FirstName { get; set; }
+        string MiddleName { get; set; }
+        string LastName { get; set; }
+        DateTime BirthDate { get; set; }
+        string Nationality { get; set; }
+        Gender Gender { get; set; }
+    }
+
+    //public class Person : IPerson
+    //{
+    //    public string firstName { get; set; }
+    //    public string middleName { get; set; }
+    //    public string lastName { get; set; }
+    //    public DateTime birthDate { get; set; }
+    //    public string nationality { get; set; }
+    //    public Gender gender { get; set; }
+    //}
+}
---
+++ TennisTournament/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisTournament
+{
+    class Player : IPerson
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Nationality { get; set; }
+        public Gender Gender { get; set; }
+
+        public int GetAge()
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - BirthDate.Year;
+
+            if (BirthDate > today.AddYears(-age))
+            {
+                age--;
+            }
+        }
+    }
+}
